1816055ee5b48c187a34fe105ae2a775dc175e09
Nuo <codypham5@gmail.com>
2022-10-03 14:59:44 -0400

    Adjusted timeblocks on HTML to display correctly. Started adding js to check the time events.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-//Moment
+//Variable for current day using Moment
 var today = moment().format('dddd, MMMM Do, YYYY')
 
-$("#currentDay").html("Today is " + moment().format('dddd, MMMM Do, YYYY'));
+//Today
+$("#currentDay").html("Today is " + today);
+//$("#currentDay").html("Today is " + moment().format('dddd, MMMM Do, YYYY'));
 
+//Testing
 console.log(today);
 
+//Check if event is in the past, present, or future
+var timeCheck = function(){
+    var currentHour = moment().hour();
+
+    console.log(currentHour);
+
+    $(".time-block").each(function(){
+        var eventTime = parseInt($(this).attr("id"));
+
+        //Testing
+        console.log(eventTime);
+    })
+}
+
+timeCheck();
